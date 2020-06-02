091844effb8d0254acb625164f0509d57b75e455
raozhipeng <superleo.rao@hotmail.com>
2020-06-02 18:43:54 +0800

    first commit

---
+++ Portal/Base64Convert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal
+{
+    public class Base64Convert
+    {
+        public static String FileToBase64(Stream fs)
+        {
+            String strRet = String.Empty;
+            try
+            {
+                if (fs == null)
+                    return strRet;
+                byte[] bt = new byte[fs.Length];
+                fs.Read(bt, 0, bt.Length);
+                strRet = Convert.ToBase64String(bt);
+                fs.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return strRet;
+        }
+    }
+}
---
+++ Portal/Controllers/PDFConvertController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PDFConvertController : ControllerBase
+    {
+        [HttpPost]
+        public String Post([FromForm]IFormCollection files)
+        {
+            if (files.Files.Count == 0)
+                throw new ArgumentNullException("file can't be null");
+            using(Stream fs=files.Files[0].OpenReadStream())
+            {
+                var str= Base64Convert.FileToBase64(fs);
+                return str;
+            }
+        }
+    }
+}
